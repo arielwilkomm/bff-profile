cad4b46070e23d72c79a69cb28d5eedbaea143c4
Ariel Wilkomm <ariel.wilkomm@compasso.com.br>
2025-03-18 18:04:31 -0300

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
-const origin = ['http://localhost:3000'];
+import * as express from 'express';
+import * as path from 'path';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -16,8 +15,6 @@ async function bootstrap() {
         }),
     );
 
-    app.enableCors({ origin, methods: 'POST,GET,PATCH,PUT,DELETE' });
-
     const config = new DocumentBuilder()
         .setTitle('BFF Profile API')
         .setDescription('Documentação da API do BFF Profile')
@@ -27,6 +24,9 @@ async function bootstrap() {
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api/docs', app, document);
+
+    const swaggerFilePath = path.join(__dirname, '../swagger', 'swagger.yaml');
+    app.use('/swagger/swagger.yaml', express.static(swaggerFilePath));
 
     await app.listen(8090);
 }
