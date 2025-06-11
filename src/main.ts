f1edceff59cc4f150840b156790876eda5da210b
Ariel Wilkomm <ariel.wilkomm@compasso.com.br>
2025-06-11 11:26:17 -0300

    create bff

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,10 @@ import { BusinessExceptionFilter } from './common/filter/business.filter';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+        origin: ['http://localhost:3000'],
+        credentials: true,
+    });
     app.useGlobalFilters(new BusinessExceptionFilter());
     app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
     app.useGlobalPipes(
