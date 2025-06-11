ec168c800da49a4581bbef8efd899baafa5ab0bd
Ariel Wilkomm <ariel.wilkomm@compasso.com.br>
2025-06-11 14:58:47 -0300

    create bff

--- test/address/clients/address.client.impl.test.ts
+++ test/address/clients/address.client.impl.test.ts
@@ -14,6 +14,7 @@ describe('AddressClientImpl', () => {
     const cpf = '123';
     const addressId = 'id';
     const dto: AddressRecordDTO = {
+        id: addressId,
         street: 'A',
         city: 'B',
         state: 'C',
